f219b05ca2f9f465d74a0d409870b72ae00c8b19
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-01-07 02:35:02 +0530

    minor changes

--- classifier/models.py
+++ classifier/models.py
@@ -3,8 +3,3 @@ from django.db import models
 
 class Upload(models.Model):
     src = models.URLField()
-
-
-class Ans(models.Model):
-    logo_border = models.CharField(max_length=500)
-    dominant_color = models.CharField(max_length=50)
